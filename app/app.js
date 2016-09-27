fcfd4d474bef6bcd28e412afb35a12fc2ed4c3d7
Brian Soumakian <bsoumakian@gmail.com>
2016-09-27 15:14:46 -0700

    integrate fast click

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
+import fs from 'fastclick';
 import firebase from 'firebase';
 import Game from './game';
+
+fs.FastClick.attach(document.body);
 
 const firebaseInst = firebase.initializeApp({
   apiKey: 'AIzaSyAV7icWQMVNXWsqhXSLNb_Zl-fICNc6sFw',
