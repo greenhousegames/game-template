f8c10ff4e22886a1816a72bb330d554189bf452f
Brian Soumakian <bsoumakian@gmail.com>
2016-09-27 15:12:53 -0700

    0.0.1

---
+++ dist/index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _boot = require('./states/boot');
+
+var _boot2 = _interopRequireDefault(_boot);
+
+var _menu = require('./states/menu');
+
+var _menu2 = _interopRequireDefault(_menu);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Game = function (_Phaser$Game) {
+  _inherits(Game, _Phaser$Game);
+
+  function Game(firebase, assetPath) {
+    _classCallCheck(this, Game);
+
+    var _this = _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).call(this, window.innerWidth, window.innerHeight, Phaser.AUTO, 'game'));
+
+    assetPath = assetPath || '/';
+
+    _this.data = {
+      name: 'game-name',
+      assetPath: assetPath.endsWith('/') ? assetPath : assetPath + '/',
+      ads: {
+        ima3: 'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-games-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'
+      }
+    };
+
+    _this.firebase = firebase;
+
+    _this.state.add('boot', _boot2.default);
+    _this.state.add('menu', _menu2.default);
+    return _this;
+  }
+
+  _createClass(Game, [{
+    key: 'resizeDevice',
+    value: function resizeDevice() {
+      this.scale.setGameSize(window.innerWidth, window.innerHeight);
+    }
+  }]);
+
+  return Game;
+}(Phaser.Game);
+
+module.exports = Game;
---
+++ dist/states/menu.js
@@ -0,0 +1,51 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var MenuState = function (_Phaser$State) {
+  _inherits(MenuState, _Phaser$State);
+
+  function MenuState(game) {
+    _classCallCheck(this, MenuState);
+
+    return _possibleConstructorReturn(this, (MenuState.__proto__ || Object.getPrototypeOf(MenuState)).call(this, game));
+  }
+
+  _createClass(MenuState, [{
+    key: 'preload',
+    value: function preload() {
+      this.game.data.score = false;
+      this.game.load.atlas(this.game.data.name, this.game.data.assetPath + this.game.data.name + '.png', this.game.data.assetPath + this.game.data.name + '.json');
+    }
+  }, {
+    key: 'create',
+    value: function create() {
+      this.game.stage.backgroundColor = '#ffffff';
+
+      this.homeButton = this.game.add.sprite(this.game.width / 2, this.game.height / 2, this.game.data.name, 'home');
+      this.homeButton.anchor.setTo(0.5, 0.5);
+      this.homeButton.inputEnabled = true;
+      this.homeButton.tint = 0x000000;
+      this.homeButton.events.onInputDown.add(function () {
+        window.location = 'http://www.greenhousegames.com';
+      }, this);
+    }
+  }, {
+    key: 'shutdown',
+    value: function shutdown() {}
+  }]);
+
+  return MenuState;
+}(Phaser.State);
+
+exports.default = MenuState;
