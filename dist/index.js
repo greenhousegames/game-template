6c4165108a04541d58185c49a6f2fb32cdb465f5
Brian Soumakian <bsoumakian@gmail.com>
2016-09-29 13:29:09 -0700

    0.0.2

--- dist/index.js
+++ dist/index.js
@@ -29,7 +29,7 @@ var Game = function (_Phaser$Game) {
     assetPath = assetPath || '/';
 
     _this.data = {
-      name: 'game-name',
+      name: 'game-template',
       assetPath: assetPath.endsWith('/') ? assetPath : assetPath + '/',
       ads: {
         ima3: 'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-games-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'
